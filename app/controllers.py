5719c1a82559315e4c42de7e8044c62145303bfe
yulia <kosheleva.yuliya.mmf@gmail.com>
2019-09-13 14:40:15 +0500

    some tg processing

--- app/controllers.py
+++ app/controllers.py
@@ -6,11 +6,20 @@ from flask import request, jsonify
 from app.utils.utils import debug
 from app.utils.bot import Bot
 
-
 def route_handler(app):
 
     @app.route('/tg/<token>', methods=['POST'])
     def telegram_callback_handler(token):
+        bot = Bot(environ.get('TELEGRAM_BOT_ID'), environ.get('TELEGRAM_API_KEY'))
+        data = request.get_json(force=True)
+        logging.error(data)
+        if 'text' in data['message']:
+            message = data['message']['text']
+            fields = message.split(' ')
+            if fields[0] == '/баг' or fields[0] == '/bug':
+                if fields[1][0] == "@":
+                    bot.send_message(data['message']['chat']['id'], 'Создаю для %s карточку О_о' % (fields[1]))
+        logging.error(token)
         return '', 200
 
     @app.route('/jira/callback', methods=['POST'])
